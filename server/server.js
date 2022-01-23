c0efb725292d5a8bae23ff6caf8696676973cea6
lewinnn <[Your>
2022-01-23 12:51:05 +0700

    [BE][Config database]

--- server/server.js
+++ server/server.js
@@ -1,12 +1,16 @@
 require('dotenv').config();
-const express = require('express');
+const express = require('express')
+const bodyParser = require('body-parser')
 const cors = require('cors')
-const app = express();
+const morgan = require('morgan')
 
+const app = express()
+app.use(morgan('combined'))
+app.use(bodyParser.json())
 app.use(cors());
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
-app.listen(()=>{
+app.listen(() => {
     console.log('Server is running on PORT', PORT);
 })
